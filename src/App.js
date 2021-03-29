f666bd5e7034d03facc7cd2a5e03a5ab2a8150d5
parth-451D <parth.dalsaniya2001@gmail.com>
2021-03-29 15:58:35 +0530

    added curousal and scrolldown0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import MyNavbar from './components/navbar/MyNavbar';
+import MyCarousal from './components/carousal/MyCarousal';
+import TitleMessage from "./components/title/TitleMessage";
+
 
 const App = () => {
   return (
     <div className="App">
     <MyNavbar />
-    
+    <MyCarousal />
+    <TitleMessage/>
+
     </div>
   )
 }
