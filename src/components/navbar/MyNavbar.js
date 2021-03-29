aa062846513d94e45be31a5ab296f14e69be9b8a
parth-451D <parth.dalsaniya2001@gmail.com>
2021-03-29 12:44:32 +0530

    initial commit navbar added

---
+++ src/components/navbar/MyNavbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "./MyNavbar.css";
+
+const MyNavbar = () => {
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="md"
+        // bg="dark"
+        variant="dark"
+        className="animate-navbar nav-theme justify-content-between"
+        fixed="top"
+      >
+        <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#About">About</Nav.Link>
+            <Nav.Link href="#Skills">Skills</Nav.Link>
+            <Nav.Link href="#Experience">Experience</Nav.Link>
+            <Nav.Link href="#Projects">Projects</Nav.Link>
+            <Nav.Link href="#Contact Me">Contact ME</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+};
+
+export default MyNavbar;
