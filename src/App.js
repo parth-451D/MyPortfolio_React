e08e6d932ca359b0068aaf3d54a07f0662e24e83
parth-451D <parth.dalsaniya2001@gmail.com>
2021-03-30 18:36:18 +0530

    added experience  page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import TitleMessage from "./components/title/TitleMessage";
 import { Container } from "react-bootstrap";
 import About from "./pages/about/About";
 import Skills from "./pages/skills/Skills";
+import Experience from "./pages/experience/Experience";
+import Slide from "react-reveal/Slide";
 
 const App = () => {
   return (
@@ -13,16 +15,37 @@ const App = () => {
       <MyNavbar />
       <MyCarousal />
       <TitleMessage />
+      <hr />
+
+      {/* about page */}
       <div>
         <Container className="container-box rounded">
+          <Slide bottom duration={700}>
             <About />
+          </Slide>
         </Container>
       </div>
+      <hr />
+
+      {/* skills page */}
       <div>
         <Container className="container-box rounded">
+          <Slide bottom duration={700}>
             <Skills />
+          </Slide>
         </Container>
       </div>
+      <hr />
+
+      {/* experience page */}
+      <div>
+        <Container className="container-box rounded">
+          <Slide bottom duration={700}>
+            <Experience />
+          </Slide>
+        </Container>
+      </div>
+      <hr />
     </div>
   );
 };
---
+++ src/pages/experience/Experience.js
@@ -0,0 +1,78 @@
+import React from "react";
+import "./experience.css";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Tilt from "react-tilt";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+// import { logo } from "../../assets/img/experience/logo.png";
+import L_ACCENTURE from "../../assets/img/experience/accenture-3.svg";
+
+const Experience = () => {
+  return (
+    <div id="Experience">
+      <h1 className="pt-3 text-center font-details-b pb-3">EXPERIENCE</h1>
+      <Jumbotron className="jumbo-style">
+        <Container>
+          <Tilt options={{ max: 25 }}>
+            <Card>
+              <Card.Header
+                as="h5"
+                className="d-flex justify-content-center flex-wrap"
+              >
+                <Card.Img
+                  variant="top"
+                  className="img-resize"
+                  src={L_ACCENTURE}
+                  alt="Accenture logo"
+                />
+              </Card.Header>
+              <Card.Body className="d-flex justify-content-center flex-column">
+                <div>
+                  <Card.Title className="text-center">
+                    Web Development Intern
+                  </Card.Title>
+                </div>
+                <div>
+                  <Card.Text className="text-center style">
+                    <strong className="body-title-style ">
+                      Front-End Developer
+                    </strong>
+                    <br />
+                    <strong>Technology:</strong> HTML, CSS, JavaScript and ReactJs.
+                    <br />
+                    <strong>Duration:</strong> March 2021 - Present
+                    <br />
+                    <strong> Description </strong>
+                    <ul className="text-left">
+                      <li>
+                        <strong>Developed &amp; enhanced</strong> multiple
+                        features with customizability option across web
+                        application.
+                      </li>
+                      <li>
+                        <strong>Developed</strong> an front-end of the main company website and developed 
+                        many features.
+                      </li>
+                      {/* <li>
+                        <strong>Provided</strong> application maintenance while
+                        working as `Production Support`.
+                      </li> */}
+                      {/* <li>
+                        <strong>Performed</strong> CRUD operations on multiple
+                        databases to load/ remove data according to the business
+                        requirements.
+                      </li> */}
+                      {/* <li><strong>Co-created</strong> React Document used as a guide for new developers.</li> */}
+                    </ul>
+                  </Card.Text>
+                </div>
+              </Card.Body>
+            </Card>
+          </Tilt>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Experience;
